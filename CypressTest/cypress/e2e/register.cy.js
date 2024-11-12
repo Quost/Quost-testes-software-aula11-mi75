ac24e2faa3b34924047a3a42d1783767b66a5af5
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2024-11-12 19:41:27 -0300

    Alterado para teste questionario

--- CypressTest/cypress/e2e/register.cy.js
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://github.com/CarlosCAK')
-    cy.get('.js-details-target > .Button-content > .Button-label').click()
-    cy.get('.HeaderMenu-link--sign-up').click()
-    
-  })
-})
